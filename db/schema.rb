5a399d248ee585e5a602664a80dcc3a03ab508a3
frechg <galenfrechette@gmail.com>
2022-03-01 16:04:33 +0700

    Removes article comments from app.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_15_073506) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_01_085523) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -19,15 +19,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_15_073506) do
     t.string "status"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "article_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "status"
-    t.index ["article_id"], name: "index_comments_on_article_id"
-  end
-
-  add_foreign_key "comments", "articles"
 end
