c3c71b481b4ce565d3ca13e97d1f3d1a20770330
frechg <galenfrechette@gmail.com>
2022-04-20 23:15:54 +0700

    Removes body validation for articles.

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,7 +8,6 @@ class Article < ApplicationRecord
   accepts_nested_attributes_for :frames, allow_destroy: true
 
   validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
   validates_associated :frames
 
   def list_authors
